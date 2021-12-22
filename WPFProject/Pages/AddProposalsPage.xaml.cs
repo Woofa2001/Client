9a15e6d4d73fc6aa524780c19da76687863f8de6
Кирилл Горошко <Woofa.1@yandex.ru>
2021-12-22 21:52:51 +0300

    Home

--- WPFProject/Pages/AddProposalsPage.xaml.cs
+++ WPFProject/Pages/AddProposalsPage.xaml.cs
@@ -40,7 +40,7 @@ namespace WPFProject.Pages
                 GridReal.Width = new GridLength(0);
                 GridProposals.Width = new GridLength(1, GridUnitType.Star);
             }
-            if(steps == 2)
+            if (steps == 2)
             {
                 LabelSteps.Content = "Шаг 3 - Выберите покупателя/дату/комиссию";
                 NextStepButton.Content = "Завершить";
@@ -48,18 +48,187 @@ namespace WPFProject.Pages
                 GridCustomers.Width = new GridLength(1, GridUnitType.Star);
                 ChangeColumn.Width = new GridLength(150);
             }
-            if(steps == 3)
+            if (steps == 3)
             {
                 var A = new Data.DEALS();
-                A.PEOPLE1= (Data.PEOPLE)PeopleDataGrid.SelectedItem;
+                A.PEOPLE1 = (Data.PEOPLE)PeopleDataGrid.SelectedItem;
                 A.PROPOSALS = (Data.PROPOSALS)ProposalsDataGrid.SelectedItem;
                 A.PEOPLE = (Data.PEOPLE)PeopleDataGrid1.SelectedItem;
                 A.COMM_REAL = Int32.Parse(RealCommTextBox.Text);
                 A.DATA_DEALS = DateTime.Parse(DateDealsPicker.Text);
                 SourceCore.DB.DEALS.Add(A);
                 SourceCore.DB.SaveChanges();
-             //   Pages.DealsPage.Close
+                //   Pages.DealsPage.Close
             }
+        }
+
+        private void PeopleFilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textbox = sender as TextBox;
+            switch (PeopleFilterComboBox.SelectedIndex)
+            {
+                case 0:
+                    PeopleDataGrid.ItemsSource = SourceCore.DB.PEOPLE.Where(filtercase => filtercase.SURNAME.Contains(textbox.Text)).ToList();
+                    break;
+                case 1:
+                    PeopleDataGrid.ItemsSource = SourceCore.DB.PEOPLE.Where(filtercase => filtercase.NAME.Contains(textbox.Text)).ToList();
+                    break;
+                case 2:
+                    PeopleDataGrid.ItemsSource = SourceCore.DB.PEOPLE.Where(filtercase => filtercase.MIDNAME.Contains(textbox.Text)).ToList();
+                    break;
+            }
+        }
+
+        private void PeopleFilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<String> Columns = new List<string>();
+            for (int I = 0; I < 3; I++)
+            {
+                Columns.Add(PeopleDataGrid.Columns[I].Header.ToString());
+            }
+            PeopleFilterComboBox.ItemsSource = Columns;
+            PeopleFilterComboBox.SelectedIndex = 0;
+            foreach (DataGridColumn Column in PeopleDataGrid.Columns)
+            {
+                Column.CanUserSort = false;
+            }
+
+            List<String> ColumnsProposals = new List<string>();
+            for (int J = 0; J < 11; J++)
+            {
+                ColumnsProposals.Add(ProposalsDataGrid.Columns[J].Header.ToString());
+            }
+            ProposalsFilterComboBox.ItemsSource = ColumnsProposals;
+            ProposalsFilterComboBox.SelectedIndex = 0;
+            foreach (DataGridColumn ColumnProposals in ProposalsDataGrid.Columns)
+            {
+                ColumnProposals.CanUserSort = false;
+            }
+
+            List<String> ColumnsPeople = new List<string>();
+            for (int K = 0; K < 3; K++)
+            {
+                ColumnsPeople.Add(PeopleDataGrid1.Columns[K].Header.ToString());
+            }
+            People1FilterComboBox.ItemsSource = ColumnsPeople;
+            People1FilterComboBox.SelectedIndex = 0;
+            foreach (DataGridColumn ColumnPeople in PeopleDataGrid1.Columns)
+            {
+                ColumnPeople.CanUserSort = false;
+            }
+
+        }
+
+        private void ProposalsFilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((ProposalsFilterComboBox.SelectedIndex == 8) || (ProposalsFilterComboBox.SelectedIndex == 9) || (ProposalsFilterComboBox.SelectedIndex == 11))
+            {
+                ProposalsFilterTextBox1.Visibility = Visibility.Visible;
+                ProposalsFilterTextBox.Width = 85;
+            }
+            else
+            {
+                ProposalsFilterTextBox1.Visibility = Visibility.Hidden;
+                ProposalsFilterTextBox.Width = 175;
+            }
+
+        }
+
+        private void ProposalsFilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textbox = sender as TextBox;
+            switch (ProposalsFilterComboBox.SelectedIndex)
+            {
+                case 0:
+                    ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.Where(filtercase => filtercase.PEOPLE.SURNAME.Contains(textbox.Text)).ToList();
+                    break;
+                case 1:
+                    ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.Where(filtercase => filtercase.TYPE_OBJECTS.NAME_TYPE.Contains(textbox.Text)).ToList();
+                    break;
+                case 2:
+                    ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.Where(filtercase => filtercase.AREAS.TYPE_AREA.Contains(textbox.Text)).ToList();
+                    break;
+                case 3:
+                    ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.Where(filtercase => filtercase.STREET.Contains(textbox.Text)).ToList();
+                    break;
+                case 4:
+                    ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.Where(filtercase => filtercase.FLAT.Value.ToString().Contains(textbox.Text)).ToList();
+                    break;
+                case 5:
+                    ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.Where(filtercase => filtercase.FLOOR.ToString().Contains(textbox.Text)).ToList();
+                    break;
+                case 6:
+                    ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.Where(filtercase => filtercase.COUNT_FLOORS.ToString().Contains(textbox.Text)).ToList();
+                    break;
+                case 7:
+                    ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.Where(filtercase => filtercase.COUNT_ROOMS.ToString().Contains(textbox.Text)).ToList();
+                    break;
+                case 8:
+                    // ProposalsFilterTextBox1.Visibility = Visibility.Visible;
+                    var textbox1 = ProposalsFilterTextBox1;
+
+                    if ((ProposalsFilterTextBox1.Text != "") || (ProposalsFilterTextBox.Text != ""))
+                    {
+                        int.TryParse(ProposalsFilterTextBox.Text, out int val);
+                        int.TryParse(ProposalsFilterTextBox1.Text, out int val1);
+
+                        //ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.Select(proposal => proposal.TOTAL_AREA.Value >= val && proposal.TOTAL_AREA.Value <= val1).ToList();
+
+                        if ((ProposalsFilterTextBox.Text != "") && (ProposalsFilterTextBox1.Text == ""))
+                        {
+                            ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.Where(filtercase => filtercase.TOTAL_AREA.Value >= val).ToList();
+                        }
+                        else if ((ProposalsFilterTextBox1.Text != "") && (ProposalsFilterTextBox.Text == ""))
+                        {
+                            ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.Where(filtercase => filtercase.TOTAL_AREA.Value <= val1).ToList();
+                        }
+                        else
+                        {
+                            ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.Where(filtercase => (filtercase.TOTAL_AREA.Value >= val) && (filtercase.TOTAL_AREA.Value <= val1)).ToList();
+                        }
+
+
+                    }
+                    else
+                        ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.ToList();
+                    break;
+                case 9:
+                    //   ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.Where(filtercase => filtercase.LIVING_AREA.Contains(textbox.Text)).ToList();
+                    break;
+                case 10:
+                    ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.Where(filtercase => filtercase.DESCRIPTION.Contains(textbox.Text)).ToList();
+                    break;
+                case 11:
+                    // ProposalsDataGrid.ItemsSource = SourceCore.DB.PROPOSALS.Where(filtercase => filtercase.COST.Contains(textbox.Text)).ToList();
+                    break;
+            }
+        }
+
+        private void People1FilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textbox = sender as TextBox;
+            switch (People1FilterComboBox.SelectedIndex)
+            {
+                case 0:
+                    PeopleDataGrid1.ItemsSource = SourceCore.DB.PEOPLE.Where(filtercase => filtercase.SURNAME.Contains(textbox.Text)).ToList();
+                    break;
+                case 1:
+                    PeopleDataGrid1.ItemsSource = SourceCore.DB.PEOPLE.Where(filtercase => filtercase.NAME.Contains(textbox.Text)).ToList();
+                    break;
+                case 2:
+                    PeopleDataGrid1.ItemsSource = SourceCore.DB.PEOPLE.Where(filtercase => filtercase.MIDNAME.Contains(textbox.Text)).ToList();
+                    break;
+            }
+        }
+
+        private void People1FilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
